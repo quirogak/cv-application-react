0ef80d1d4e4467b7a8478d12e7f6944d421277e0
quirogak <quirogakill@gmail.com>
2023-06-09 12:34:02 -0500

    Added a method to render lists.

--- src/components/section.js
+++ src/components/section.js
@@ -4,13 +4,30 @@ import "../styles/section.css"
 class Section extends Component {
     constructor(props) {
         super(props)
+        this.getContent = this.getContent.bind(this)
+        this.renderList = this.renderList.bind(this)
     }
+
+    renderList(listArr) {
+        return (<ul>{listArr.map(el => <li>{el}</li>)}</ul>)
+    }
+
+    getContent() {
+        const content = this.props.content
+        if (content) {
+            const currentValue = content.value
+            if (typeof (currentValue) === "object") // when the current value is a list.
+                return this.renderList(currentValue)
+            return currentValue
+        }
+        else return
+    }
+
     render() {
         return (
             <section>
-                <div className="title">{this.props.title}</div>
-                <div className="info">
-                </div>
+                <div className="title">{this.props.title} <hr></hr></div>
+                <div className="content">{this.getContent()}</div>
             </section>
         )
     }
