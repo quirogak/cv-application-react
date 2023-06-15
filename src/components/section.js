f772ed2510b5c90134cd345bc4d7ea522651687a
quirogak <quirogakill@gmail.com>
2023-06-15 16:57:02 -0500

    Added an edit button on hovering each section. I had to use state, in order to save the current content of the section, but also adding a new button component. The logic made in getContent, allows us to add as many extra elements in state as we want to add on event handler, because it's done using map and recursive methods.

--- src/components/section.js
+++ src/components/section.js
@@ -1,29 +1,52 @@
 import { Component } from "react";
 import "../styles/section.css"
 import List from "./list";
+import EditBtn from "./editButton";
 
 class Section extends Component {
     constructor(props) {
         super(props)
         this.getContent = this.getContent.bind(this)
+        this.genEditButton = this.genEditButton.bind(this)
+
+        this.state = {
+            currentContent: this.props.content //aqui puedo ingresar el contenido que quiero, y también empezar a hacer la lógica para que ese contenido aparezca junto con el botón de Edit.
+        }
     }
 
-    getContent() {
-        const content = this.props.content
-        if (content) {
-            const currentValue = content.value
+    getContent(content) {
+        const currentValue = content.value
+
+        //if the content is multi-dimensional, we call getContent recursively on each element, so they can be returned using the logic below.
+        if (content.length > 1)
+            return content.map(el => {
+                return this.getContent(el)
+            })
+
+        //when the content has a value.
+        if (currentValue)
             if (typeof (currentValue) === "object") // when the current value is a list.
                 return <List content={currentValue}></List>
-            return currentValue
-        }
-        else return
+            else return currentValue
+
+        //when the content is directly a component.
+        else return content
+    }
+
+    genEditButton() {
+        const content = this.props.content
+        const contentWithButton = [content, <EditBtn></EditBtn>]
+        this.setState({
+            currentContent: contentWithButton
+        })
     }
 
     render() {
+        const className = this.props.title.toLowerCase()
         return (
-            <section className={(this.props.title).toLowerCase()}>
+            <section onMouseEnter={() => this.genEditButton()} className={(className)}>
                 <div className="title">{this.props.title} <hr></hr></div>
-                <div className="content">{this.getContent()}</div>
+                <div className="content">{this.getContent(this.state.currentContent)}</div>
             </section>
         )
     }
