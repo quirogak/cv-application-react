96655ef09478c2e42349990ee3c211567f546549
quirogak <quirogakill@gmail.com>
2023-06-18 13:18:46 -0500

    passed contentHandler to change section's state. this allows to manipulate the content displayed on each section dinamically.

--- src/components/editButton.js
+++ src/components/editButton.js
@@ -4,10 +4,16 @@ import "../styles/buttons.css"
 class EditBtn extends Component {
     constructor(props) {
         super(props)
+        this.parentClass = this.props.parentClass
+        this.elementsToInput = this.elementsToInput.bind(this)
+    }
+
+    elementsToInput(elements) {
+        this.props.contentHandler(<input defaultValue={elements[1]}></input>)
     }
 
     render() {
-        return <button className={[this.props.parentClass, "editBtn"].join(" ")}>Edit</button>
+        return <button className={[this.parentClass, "editBtn"].join(" ")} onClick={() => this.elementsToInput(this.props.sectionContent)}>Edit</button>
     }
 }
 
