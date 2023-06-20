be6e5fa89c8abafe4814eadf2d41bc3d4d8071da
quirogak <quirogakill@gmail.com>
2023-06-20 11:31:51 -0500

    Passed titleHandler as prop to change main state.

--- src/components/section.js
+++ src/components/section.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import "../styles/section.css"
 import List from "./list";
 import EditBtn from "./editButton";
+import uniqid from "uniqid"
 
 class Section extends Component {
     constructor(props) {
@@ -10,17 +11,18 @@ class Section extends Component {
         this.genEditButton = this.genEditButton.bind(this)
         this.removeButtons = this.removeButtons.bind(this)
         this.contentHandler = this.contentHandler.bind(this)
+        this.titleHandler = this.titleHandler.bind(this)
 
         this.className = this.props.title.toLowerCase()
 
         this.state = {
-            currentContent: this.props.content //aqui puedo ingresar el contenido que quiero, y también empezar a hacer la lógica para que ese contenido aparezca junto con el botón de Edit.
+            currentContent: this.props.content, //default value
+            currentTitle: this.props.title
         }
     }
 
     getContent(content) {
         const currentValue = content.value
-
         //if the content is multi-dimensional, we call getContent recursively on each element, so they can be returned using the logic below.
         if (content.length > 1)
             return content.map(el => {
@@ -30,7 +32,7 @@ class Section extends Component {
         //when the content has a value.
         if (currentValue)
             if (typeof (currentValue) === "object") // when the current value is a list.
-                return <List content={currentValue}></List>
+                return <List key={uniqid()} content={currentValue}></List>
             else return currentValue
 
         //when the content is directly a component.
@@ -40,7 +42,12 @@ class Section extends Component {
     genEditButton() {
         if (typeof (this.state.currentContent[1]) === "object") return // avoid generating duplicated edit buttons.
         const content = this.state.currentContent
-        const contentWithButton = [content, <EditBtn sectionContent={[this.className, content.value]} contentHandler={this.contentHandler}></EditBtn>]
+        const contentWithButton = [
+            content,
+            <EditBtn key={uniqid()} sectionContent={[this.className, content.value]}
+                contentHandler={this.contentHandler} titleHandler={this.titleHandler}>
+            </EditBtn>
+        ]
         this.setState({
             currentContent: contentWithButton
         })
@@ -58,12 +65,16 @@ class Section extends Component {
         })
     }
 
-    // titleHandler() {}
+    titleHandler(content) {
+        this.setState({
+            currentTitle: content
+        })
+    }
 
     render() {
         return (
             <section onMouseEnter={this.genEditButton} onMouseLeave={this.removeButtons} id={this.className} className={this.className}>
-                <div className="title">{this.props.title} <hr></hr></div>
+                <div className="title">{this.state.currentTitle} <hr></hr></div>
                 <div className="content">{this.getContent(this.state.currentContent)}</div>
             </section>
         )
