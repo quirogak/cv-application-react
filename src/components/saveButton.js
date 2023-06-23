97da9e1a6c0651703bd8e248ad08711dd2c94ec2
quirogak <quirogakill@gmail.com>
2023-06-23 14:09:54 -0500

    Fixed bug, contentHandler wasn't rendering values. because the parameters inside of contentHandler weren't being passed correctly.

--- src/components/saveButton.js
+++ src/components/saveButton.js
@@ -8,13 +8,12 @@ class SaveBtn extends Component {
 
         this.getInputValues = this.getInputValues.bind(this)
         this.inputToElements = this.inputToElements.bind(this)
-        this.parentClass = this.props.sectionContent[0]
+        this.parentClass = this.props.className
     }
 
     getInputValues(parentClass) {
         const titleParent = document.getElementsByClassName("title " + parentClass)[0]
         const contentParent = document.getElementsByClassName("content " + parentClass)[0]
-
         const titleChildren = titleParent.childNodes[0].value
         const contentChildren = []
 
@@ -23,13 +22,16 @@ class SaveBtn extends Component {
             contentChildren.push(element)
         }
 
-        return [titleChildren, contentChildren]
+        if (contentChildren.length > 1) // when the contentChildren is a list of elements.
+            return [titleChildren, contentChildren]
+
+        return [titleChildren, contentChildren[0]]
     }
 
     inputToElements() {
         const inputValues = this.getInputValues(this.parentClass)
         this.props.titleHandler(inputValues[0])
-        this.props.contentHandler(inputValues[1])
+        this.props.contentHandler({ value: inputValues[1] })
         this.props.addHandlers() // return original section mouseEvent handlers.
     }
 
